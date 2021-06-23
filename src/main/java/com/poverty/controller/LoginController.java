631defecba812bcfe0f7dee8546a52dd9f4a9180
chocolili <1762535561@qq.com>
2021-06-23 19:43:42 +0800

    chocolili Add LoginService

--- src/main/java/com/poverty/controller/LoginController.java
+++ src/main/java/com/poverty/controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.poverty.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Li
  * @version 1.0
  * @date Created in 2021/6/23 19:35
  */
+@RestController
 public class LoginController {
 }
