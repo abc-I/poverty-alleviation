828ea0dfe0d424e4533b293177e82ef772edf7c9
abc-I <1773530070@qq.com>
2021-06-23 19:38:49 +0800

    abc-I Add LoginController;Update User

--- src/main/java/com/poverty/service/impl/LoginServiceImpl.java
+++ src/main/java/com/poverty/service/impl/LoginServiceImpl.java
@@ -3,6 +3,9 @@ package com.poverty.service.impl;
 import com.poverty.service.LoginService;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author chocolili
+ */
 @Service
 public class LoginServiceImpl implements LoginService {
 }
